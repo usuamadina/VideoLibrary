2448b8bb2205ed693132ae9869aa8ead9fbf4022
Usua Madina <u.madina.i@gmail.com>
2017-07-04 18:20:50 +0200

    Function to load Json's from a resource

--- tv/src/main/java/com/example/videolibrary/Utils.java
+++ tv/src/main/java/com/example/videolibrary/Utils.java
@@ -82,5 +82,24 @@ public class Utils {
         return result;
     }
 
-
+    public static String loadJSONFromResource(Context context, int resource) {
+        if (resource <= 0) return null;
+        String json = null;
+        InputStream is = context.getResources().openRawResource(resource);
+        try {
+            if (is != null) {
+                int size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                json = new String(buffer, "UTF-8");
+            }
+        } catch (IOException e) {
+        } finally {
+            try {
+                if (is != null) is.close();
+            } catch (IOException e) {
+            }
+        }
+        return json;
+    }
 }
